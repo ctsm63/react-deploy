be9d923124ba8718b679c462afa3a99da179d55d
ctsm63 <ctsm63@yahoo.fr>
2024-11-11 13:58:34 +0100

    code source

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import logo from './giphy.webp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="scrum" />
+      <h1>React Scrum</h1>
+    </div>
+  );
+}
+
+export default App;
