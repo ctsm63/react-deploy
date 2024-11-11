cf79eb91b876e1eafd65a6cb9ca40dbeeb2bcce3
ctsm63 <ctsm63@yahoo.fr>
2024-11-11 14:27:16 +0100

    link école du web

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <img src={logo} alt="scrum" />
       <h1>React Scrum</h1>
+      <a href="https://youtu.be/ziPzOp6j0Oo?si=0Cdu2lpSmbYTifK3" target="_blank" rel="noreferrer">
+École du Web - React : Mise en ligne gratuite sur GitHub</a>
     </div>
   );
 }
